724ea32487da9cb37f0a52cf608ca77210ce1494
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-12-01 05:32:31 -0500

    initial commit

---
+++ src/challenges/challenge1.rs
@@ -0,0 +1,7 @@
+pub fn challenge_one_partone() {
+    println!("Hello");
+}
+
+pub fn challenge_one_parttwo() {
+    println!("Hello again")
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod challenges;
+use challenges::challenge1::{challenge_one_partone, challenge_one_parttwo};
+
+fn main() {
+    challenge_one_partone();
+}
