99f36b237d9465dc6d9132babfff0e4648f0cc53
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-12-04 06:11:19 -0500

    completed day 4 part one

--- src/challenges/challenge4.rs
+++ src/challenges/challenge4.rs
@@ -1,9 +1,25 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
 pub fn challengefour() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello");
+    let file = File::open("assets/challenge4/input.txt")?;
+    let reader = BufReader::new(file);
+    let input = reader
+        .lines()
+        .map(|x| x.ok())
+        .filter(|x| x.is_some())
+        .map(|x| x.unwrap())
+        .collect::<Vec<String>>();
+    let output = part_one(input)?;
+    println!("The output of challenge 4 first part is {}", output);
     Ok(())
 }
 
-fn part_one(input: Vec<String>) -> Result<u32, Box<dyn std::error::Error>> {
+type Board = Vec<Vec<usize>>;
+
+fn part_one(input: Vec<String>) -> Result<usize, Box<dyn std::error::Error>> {
     let draws = input[0]
         .clone()
         .split(",")
@@ -17,10 +33,40 @@ fn part_one(input: Vec<String>) -> Result<u32, Box<dyn std::error::Error>> {
             }
         })
         .collect::<Vec<usize>>();
+    let boards = &input[1..]
+        .join("\n")
+        .trim()
+        .split("\n\n")
+        .into_iter()
+        .map(|b| {
+            b.lines()
+                .map(|l| l.split_whitespace().map(|i| i.parse().unwrap()).collect())
+                .collect()
+        })
+        .collect::<Vec<Board>>();
     for i in 5..draws.len() {
-        dbg!(i);
+        let winner = boards.iter().find_map(|b| check_board(&draws[0..i], &b));
+        if let Some(score) = winner {
+            return Ok(score);
+        }
     }
-    Ok(0)
+    unreachable!()
+}
+
+fn calculate_board(draws: &[usize], board: &Board) -> usize {
+    board.iter().flatten().filter(|&x| !draws.contains(x)).sum()
+}
+
+fn check_board(draws: &[usize], board: &Board) -> Option<usize> {
+    for i in 0..5 {
+        if (0..5).all(|j| draws.contains(&board[i][j])) {
+            return Some(calculate_board(draws, board) * draws.last().unwrap());
+        }
+        if (0..5).all(|j| draws.contains(&board[j][i])) {
+            return Some(calculate_board(draws, board) * draws.last().unwrap());
+        }
+    }
+    None
 }
 
 #[cfg(test)]
