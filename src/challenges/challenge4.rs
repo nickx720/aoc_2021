538bf2bca5a4c9260801abaf5f6b1d8449d9ae49
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-12-04 13:17:35 -0500

    completed day 4 part two

--- src/challenges/challenge4.rs
+++ src/challenges/challenge4.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashSet,
     fs::File,
     io::{BufRead, BufReader},
 };
@@ -12,8 +13,10 @@ pub fn challengefour() -> Result<(), Box<dyn std::error::Error>> {
         .filter(|x| x.is_some())
         .map(|x| x.unwrap())
         .collect::<Vec<String>>();
-    let output = part_one(input)?;
-    println!("The output of challenge 4 first part is {}", output);
+    // let output = part_one(input)?;
+    // println!("The output of challenge 4 first part is {}", output);
+    let output = part_two(input)?;
+    println!("The output of challenge 4 second part is {}", output);
     Ok(())
 }
 
@@ -48,6 +51,48 @@ fn part_one(input: Vec<String>) -> Result<usize, Box<dyn std::error::Error>> {
         let winner = boards.iter().find_map(|b| check_board(&draws[0..i], &b));
         if let Some(score) = winner {
             return Ok(score);
+        }
+    }
+    unreachable!()
+}
+
+fn part_two(input: Vec<String>) -> Result<usize, Box<dyn std::error::Error>> {
+    let draws = input[0]
+        .clone()
+        .split(",")
+        .map(|x| x.parse::<usize>())
+        .filter(|x| x.is_ok())
+        .map(|x| {
+            let x = x.ok();
+            match x {
+                Some(val) => val,
+                _ => unreachable!(),
+            }
+        })
+        .collect::<Vec<usize>>();
+    let mut boards = input[1..]
+        .join("\n")
+        .trim()
+        .split("\n\n")
+        .into_iter()
+        .map(|b| {
+            b.lines()
+                .map(|l| l.split_whitespace().map(|i| i.parse().unwrap()).collect())
+                .collect()
+        })
+        .collect::<Vec<Board>>()
+        .into_iter()
+        .collect::<HashSet<_>>();
+    for i in 5..draws.len() {
+        let winners = boards
+            .iter()
+            .filter_map(|b| check_board(&draws[0..i], &b).map(|score| (b.clone(), score)))
+            .collect::<Vec<_>>();
+        for (b, _) in &winners {
+            boards.remove(b);
+        }
+        if boards.is_empty() {
+            return Ok(winners[0].1);
         }
     }
     unreachable!()
@@ -89,6 +134,21 @@ mod test {
             .collect::<Vec<String>>();
         let output = part_one(input)?;
         assert_eq!(output, 4512);
+        Ok(())
+    }
+
+    #[test]
+    fn part_two_works() -> Result<(), Box<dyn std::error::Error>> {
+        let file = File::open("assets/challenge4/sample.txt")?;
+        let reader = BufReader::new(file);
+        let input = reader
+            .lines()
+            .map(|x| x.ok())
+            .filter(|x| x.is_some())
+            .map(|x| x.unwrap())
+            .collect::<Vec<String>>();
+        let output = part_two(input)?;
+        assert_eq!(output, 1924);
         Ok(())
     }
 }
