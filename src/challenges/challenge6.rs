1ec69dd9099947bd0720393b915c26c80034be83
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-12-06 09:44:08 -0500

    day 6 second part completed

--- src/challenges/challenge6.rs
+++ src/challenges/challenge6.rs
@@ -11,8 +11,11 @@ pub fn challengesix() -> Result<(), Box<dyn std::error::Error>> {
         .filter(|x| x.is_ok())
         .map(|x| if let Ok(val) = x { val } else { unreachable!() })
         .collect::<String>();
-    let output = part_one(input);
-    println!("The output of challenge six first part is {}", output);
+    //   let output = part_one(input);
+    // println!("The output of challenge six first part is {}", output);
+    println!("Starting");
+    let output = part_two(input);
+    println!("The output of challenge six second part is {}", output);
     Ok(())
 }
 fn part_one(input: String) -> usize {
@@ -37,6 +40,35 @@ fn part_one(input: String) -> usize {
         index -= 1;
     }
     formatted_input.len()
+}
+type Fish = [u128; 9];
+fn step_day(val: Fish) -> Fish {
+    [
+        val[1],
+        val[2],
+        val[3],
+        val[4],
+        val[5],
+        val[6],
+        val[7] + val[0],
+        val[8],
+        val[0],
+    ]
+}
+fn step_days(days: usize, fish: Fish) -> Fish {
+    (0..days).fold(fish, |acc, _| step_day(acc))
+}
+fn part_two(input: String) -> u128 {
+    let formatted_input =
+        format_input(input)
+            .into_iter()
+            .map(|x| x as usize)
+            .fold([0u128; 9], |mut acc, f| {
+                acc[f] += 1;
+                acc
+            });
+
+    step_days(256, formatted_input).iter().sum()
 }
 fn format_input(input: String) -> Vec<u32> {
     input
@@ -69,6 +101,19 @@ mod tests {
             .collect::<String>();
         let output = part_one(input);
         assert_eq!(output, 5934);
+        Ok(())
+    }
+    #[test]
+    fn part_two_works() -> Result<(), Box<dyn std::error::Error>> {
+        let file = File::open("assets/challenge6/sample.txt")?;
+        let reader = BufReader::new(file);
+        let input = reader
+            .lines()
+            .filter(|x| x.is_ok())
+            .map(|x| if let Ok(val) = x { val } else { unreachable!() })
+            .collect::<String>();
+        let output = part_two(input);
+        assert_eq!(output, 26984457539);
         Ok(())
     }
 }
