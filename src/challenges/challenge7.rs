330841159b20b8f5762f4c93647c47194dc830b1
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-12-07 10:06:02 -0500

    completed day7

--- src/challenges/challenge7.rs
+++ src/challenges/challenge7.rs
@@ -1,15 +1,64 @@
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
 pub fn challengeseven() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello");
+    let file = File::open("assets/challenge7/input.txt")?;
+    let reader = BufReader::new(file);
+    let input = reader
+        .lines()
+        .map(|x| {
+            if let Ok(value) = x {
+                value
+            } else {
+                "".to_string()
+            }
+        })
+        .collect::<String>();
+    //let output = part_one(input)?;
+    //println!("The output for day 7 first part is {}", output);
+    let output = part_two(input)?;
+    println!("The output for day 7 second part is {}", output);
     Ok(())
 }
 
-fn part_one(input: String) -> Result<usize, Box<dyn std::error::Error>> {
+fn part_one(input: String) -> Result<i32, Box<dyn std::error::Error>> {
     let formatted_input = format_input(input);
-    dbg!(formatted_input);
-    Ok(2)
+    let min = *formatted_input.iter().min().unwrap();
+    let max = *formatted_input.iter().max().unwrap();
+    let output = (min..=max)
+        .map(|item| {
+            formatted_input
+                .iter()
+                .map(|val| (val - item).abs())
+                .sum::<i32>()
+        })
+        .min()
+        .unwrap();
+    Ok(output)
+}
+fn calculate_diff(n: i32) -> i32 {
+    n * (n + 1) / 2
+}
+fn part_two(input: String) -> Result<i32, Box<dyn std::error::Error>> {
+    let formatted_input = format_input(input);
+    let min = *formatted_input.iter().min().unwrap();
+    let max = *formatted_input.iter().max().unwrap();
+    let output = (min..=max)
+        .map(|item| {
+            formatted_input
+                .iter()
+                .map(|val| calculate_diff((val - item).abs()))
+                .sum::<i32>()
+        })
+        .min()
+        .unwrap();
+    Ok(output)
 }
 
-fn format_input(input: String) -> Vec<u32> {
+fn format_input(input: String) -> Vec<i32> {
     input
         .split(",")
         .map(|num| {
@@ -47,4 +96,22 @@ mod tests {
         assert_eq!(output, 37);
         Ok(())
     }
+}
+#[test]
+fn part_two_works() -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::open("assets/challenge7/sample.txt")?;
+    let reader = BufReader::new(file);
+    let input = reader
+        .lines()
+        .map(|x| {
+            if let Ok(value) = x {
+                value
+            } else {
+                "".to_string()
+            }
+        })
+        .collect::<String>();
+    let output = part_two(input)?;
+    assert_eq!(output, 168);
+    Ok(())
 }
